a32a65ecc94faf451c0bc9207a25dca9b44cd560
Marcos Henrique Brito Souza <130499268+Marcosmh7xb@users.noreply.github.com>
2024-03-21 16:46:12 -0300

    Update script.js

--- Tela De Login - Henet/js/script.js
+++ Tela De Login - Henet/js/script.js
@@ -7,7 +7,7 @@ mode.addEventListener('click', () => {
     if(mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
-        document.querySelector('img').src = "Tela_de_Login/Henet.svg";
+        document.querySelector('img').src = "Tela De Login - Henet/Henet.svg";
 
         form.classList.add('dark');
         return ;
@@ -15,7 +15,7 @@ mode.addEventListener('click', () => {
     
     mode.classList.remove('fa-sun');
     mode.classList.add('fa-moon');
-    document.querySelector('img').src = "Tela_de_Login/Henet-negativo.svg";
+    document.querySelector('img').src = "Tela De Login - Henet/Henet-negativo.svg";
 
 
     form.classList.remove('dark');
