4bc6c3ff0c1c5d3f75f237e4adcaf4663d593ad9
Marcos Henrique Brito Souza <130499268+Marcosmh7xb@users.noreply.github.com>
2024-03-23 11:12:11 -0300

    Add files via upload

---
+++ Tela De Login - Henet/js/script.js
@@ -0,0 +1,29 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('log');
+    
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        document.querySelector('img').src = "Tela De Login - Henet/img/Henet.svg";
+
+        form.classList.add('dark');
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+    document.querySelector('img').src = "Tela De Login - Henet/img/Henet-negativo.svg";
+
+
+    form.classList.remove('dark');
+
+
+});
+
+
+
+
+
